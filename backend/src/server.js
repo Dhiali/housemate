76b0d9c12081fd7a647f63ec530018a328f28635
Dhiali <231299@virtualwindow.co.za>
2025-10-30 16:15:05 +0200

    Fix syntax error at end of server.js (remove stray bracket)

--- backend/src/server.js
+++ backend/src/server.js
@@ -99,6 +99,7 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again later.',
   standardHeaders: true,
   legacyHeaders: false,
+});
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
