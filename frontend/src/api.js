1cf5b6dd7aabf41b984fa4adf25af3d65914c6e3
Dhiali <231299@virtualwindow.co.za>
2025-11-04 21:37:20 +0200

    task drop down test

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,5 +7,12 @@ const API = axios.create({
 // Example API functions
 export const getUsers = () => API.get("/users");
 export const addUser = (user) => API.post("/users", user);
-export const getTasks = () => API.get("/tasks");
+export const getTasks = (options = {}) => {
+  const params = new URLSearchParams();
+  if (options.viewAll) {
+    params.append('view_all', 'true');
+  }
+  const queryString = params.toString();
+  return API.get(`/tasks${queryString ? `?${queryString}` : ''}`);
+};
 export const addTask = (task) => API.post("/tasks", task);
